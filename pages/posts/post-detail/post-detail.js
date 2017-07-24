9082617f8ab5d5d4432c220721dd67b83f366381
calvincao <calvin.j.cao@gmail.com>
2017-07-24 18:43:39 +0800

    modify

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -10,12 +10,65 @@ Page({
   },
 
   /**
+   * self defined funtion
+   */
+
+  onCollectTap : function(event){
+      var collected = false
+      wx.getStorage({
+        key: 'collected',
+        success: function(res) {
+          console.log(res.data)
+          collected = res.data
+        
+          if (collected) {
+            console.log("already collected")
+          } else {
+            wx.showModal({
+              title: '提示',
+              content: '这是一个模态弹窗',
+              success: function (res) {
+                if (res.confirm) {
+                  console.log('用户点击确定')
+                } else if (res.cancel) {
+                  console.log('用户点击取消')
+                }
+              }
+            })
+            wx.setStorage({
+              key: 'collected',
+              data: 'true',
+            })
+          }
+        },
+      })
+
+     
+
+      
+  },
+
+  /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
      var postId = options.id
      var postData = postsdata.postList[postId]
      this.setData(postData)
+     
+     var collected = wx.getStorage({
+       key: 'collected',
+       success: function(res) {},
+     })
+
+     if(collected == undefined){
+          wx.setStorage({
+          key: 'collected',
+          data: 'false',
+        })
+     }
+ 
+     
      //console.log(postData)
   },
 
